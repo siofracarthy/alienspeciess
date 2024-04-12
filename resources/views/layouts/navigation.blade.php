82690d1187372babe246e45f1efd303aae85b622
scarthy~ <n00220387@iadt.ie>
2024-04-12 12:52:26 +0100

    sdsddsd

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,6 +22,12 @@
                         {{ __('All Species') }}
                     </x-nav-link>
                 </div>
+
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <x-nav-link :href="route('guides.index')" :active="request()->routeIs('guides.index')">
+                            {{ __('All Guides') }}
+                        </x-nav-link>
+                    </div>
             </div>
 
             <!-- Settings Dropdown -->
