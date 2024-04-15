7cc43eff6efaae83de740a90176258e263442274
siofracarthy <125364525+siofracarthy@users.noreply.github.com>
2024-04-15 01:52:57 +0100

    Update create.blade.php

--- resources/views/species/create.blade.php
+++ resources/views/species/create.blade.php
@@ -1,40 +1,48 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{-- <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Create Species') }}
-        </h2>
+        </h2> --}}
     </x-slot>
 
     <div class="py-12">
+        <h1 class="font-bold text-center text-black py-2" style="font-size: 1.9rem;">{{ __('Create Species') }}</h1>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                 <form action="{{ route('species.store') }}" method="post" enctype="multipart/form-data">
                     @csrf
-                    <x-text-input type="text" name="title" field="title" placeholder="Title.." class="w-full"
-                        autocomplete="off" :value="@old('title')"></x-text-input>
+                    <h1 class="font-bold text-black p-2" style="font-size: 1rem;">{{ __('Title:') }}</h1>
+                    <x-text-input type="text" name="title" field="title" placeholder="Title.."
+                        style="margin-bottom: 20px;" class="w-full" autocomplete="off" :value="@old('title')"></x-text-input>
 
                     <!-- I created a new component called textarea, you will need to do the same to using the x-textarea component -->
+                    <h1 class="font-bold text-black p-2" style="font-size: 1rem;">{{ __('Description:') }}</h1>
                     <x-textarea name="description" rows="10" field="description" placeholder="Description.."
-                        style="margin-bottom: 10px;" class="w-full mt-2" :value="@old('description')">
+                        style="margin-bottom: 20px;" class="w-full mt-2" :value="@old('description')">
                     </x-textarea>
 
-                    <x-text-input type="text" name="origin" field="origin" placeholder="Origin.." class="w-full"
-                    autocomplete="off" :value="@old('origin')"></x-text-input>
+                    <h1 class="font-bold text-black p-2" style="font-size: 1rem;">{{ __('Origin:') }}</h1>
+                    <x-text-input type="text" name="origin" field="origin" placeholder="Origin.."
+                        style="margin-bottom: 20px;" class="w-full" autocomplete="off" :value="@old('origin')"></x-text-input>
 
-                    <x-text-input type="text" name="habitat" field="habitat" placeholder="Habitat.." class="w-full"
-                    autocomplete="off" :value="@old('habitat')"></x-text-input>
+                        <h1 class="font-bold text-black p-2" style="font-size: 1rem;">{{ __('Habitat:') }}</h1>
+                    <x-text-input type="text" name="habitat" field="habitat" placeholder="Habitat.."
+                        style="margin-bottom: 20px;" class="w-full" autocomplete="off" :value="@old('habitat')"></x-text-input>
 
-                    <x-text-input type="text" name="sighting_year" field="sighting_year" placeholder="Sighting Year.." class="w-full"
-                    autocomplete="off" :value="@old('sighting_year')"></x-text-input>
+                        <h1 class="font-bold text-black p-2" style="font-size: 1rem;">{{ __('Sighting Year:') }}</h1>
+                    <x-text-input type="number" name="sighting_year" field="sighting_year"
+                        placeholder="Sighting Year.." style="margin-bottom: 20px;" class="w-65" autocomplete="off"
+                        :value="@old('sighting_year')"></x-text-input>
+                        <h1 class="font-bold text-black p-2" style="font-size: 1rem;">{{ __('Risk Level:') }}</h1>
 
-                    <x-text-input type="text" name="risk_level" field="risk_level" placeholder="Risk Level.." class="w-full"
-                    autocomplete="off" :value="@old('risk_level')"></x-text-input>
+                    <x-text-input type="number" name="risk_level" field="risk_level" placeholder="Risk Level.."
+                        style="margin-bottom: 20px;" class="w-65" autocomplete="off" :value="@old('risk_level')"></x-text-input>
 
                     {{-- <x-text-input type="text" name="lng" field="lng" placeholder="lng.." class="w-full"
                     autocomplete="off" :value="@old('lng')"></x-text-input> --}}
 
-                    <x-file-input type="file" name="species_image" placeholder="Species" class="w-full mt-6"
-                        field="species_image" :value="@old('species_image')">>
+                    <x-file-input type="file" name="species_image" placeholder="Species" style="margin-bottom: 20px;"
+                        class="w-full mt-6" field="species_image" :value="@old('species_image')">>
                     </x-file-input>
 
                     {{-- <div class="mt-6">
