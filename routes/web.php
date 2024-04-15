0f8b4e0274c9643ed61c347f66d716f613c5b544
siofracarthy <125364525+siofracarthy@users.noreply.github.com>
2024-04-15 01:54:40 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SpeciesController;
 use App\Http\Controllers\GuideController;
+use App\Http\Controllers\HomeController;
 
 
 /*
@@ -21,9 +22,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard', [HomeController::class, 'index'])
+->middleware(['auth', 'verified'])
+->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -35,15 +40,6 @@ Route::middleware('auth')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::resource('/species', SpeciesController::class)->only(['index', 'create', 'store', 'show', 'edit', 'destroy', 'update']);
     Route::resource('/guides', GuideController::class)->only(['index', 'create', 'store', 'show', 'edit', 'destroy', 'update']);
-    Route::get('/guides/create', [GuideController::class, 'create'])->name('guides.create');
-    Route::get('/guides/show', [GuideController::class, 'show'])->name('guides.show');
-    Route::get('/guides/index', [GuideController::class, 'index'])->name('guides.index');
-    Route::get('/guides', [GuideController::class, 'index'])->name('guides.index');
-
-
-
-
-
 
 });
 
