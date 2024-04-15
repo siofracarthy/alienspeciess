8e96ff9a264e6d05774800595b93c1650673bf07
siofracarthy <125364525+siofracarthy@users.noreply.github.com>
2024-04-15 01:50:12 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,28 +1,29 @@
 <?php
-
 namespace App\Http\Controllers;
+use App\Models\Species;
 
-use Illuminate\Http\Request;
-
-class HomeController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    class HomeController extends Controller
     {
-        $this->middleware('auth');
-    }
+        /**
+         * Create a new controller instance.
+         *
+         * @return void
+         */
+        public function __construct()
+        {
+            $this->middleware('auth');
+        }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
+        /**
+         * Show the application dashboard.
+         *
+         * @return \Illuminate\Contracts\Support\Renderable
+         */
+        public function index()
+        {
+
+            //In order from highest level of risk to lowest
+            $species = Species::orderByDesc('risk_level')->get();
+            return view('dashboard')->with('species', $species);
+        }
     }
-}
