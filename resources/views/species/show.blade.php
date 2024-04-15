a54cd6a798ae852f77708551edd9cfabb7606560
siofracarthy <125364525+siofracarthy@users.noreply.github.com>
2024-04-15 01:53:21 +0100

    Update show.blade.php

--- resources/views/species/show.blade.php
+++ resources/views/species/show.blade.php
@@ -13,32 +13,52 @@
                     <table class="table table-hover">
                         <tbody>
                             <tr>
-                                <td rowspan="12">
+                                <td rowspan="1">
                                     <div style="display: flex; justify-content: center;">
                                         <!-- use the asset function, access the file $species->species_image in the folder storage/images -->
-                                        <img src="{{ asset($species->species_image) }}" alt="{{ $species->title }}" width="600">
+                                        <img src="{{ asset($species->species_image) }}" alt="{{ $species->title }}"
+                                            width="600">
                                     </div>
                                 </td>
                             </tr>
 
 
-                                <td class="font-bold ">Title </td>
-                                <td>{{ $species->title }}</td>
-                            </tr>
+                            <div class="seperation">
 
-                            <tr>
-                                <td class="font-bold">Description </td>
-                                <td>{{ $species->description }}</td>
-                            </tr>
-                            <tr>
-                                <td class="font-bold ">Origin </td>
-                                <td>{{ $species->origin }}</td>
-                            </tr>
+                                <tr>
+                                    <td class="font-bold py-5" style="font-size: 1.70rem;">{{ $species->title }}</td>
+                                </tr>
 
-                            <tr>
-                                <td class="font-bold ">Risk Level </td>
-                                <td>{{ $species->risk_level }}</td>
-                            </tr>
+
+
+                                <tr>
+                                    <td class="font-thin" style="font-size: 1rem;">{{ $species->description }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="font-bold ">Origin: {{ $species->origin }} </td>
+                                </tr>
+
+                                <tr>
+                                    <td class="font-bold ">Risk Level: {{ $species->risk_level }} </td>
+                                </tr>
+
+                                <tr>
+                                    <td class="font-bold ">Habitat: {{ $species->habitat }} </td>
+                                </tr>
+
+                                <tr>
+                                    <td class="font-bold ">Guide:
+                                        @if ($species->guides)
+                                            @foreach ($species->guides as $guide)
+                                                {{ $guide->guide_url }}
+                                            @endforeach
+                                        @else
+                                            No habitats found.
+                                        @endif
+                                    </td>
+                                </tr>
+
+                            </div>
                         </tbody>
 
 
@@ -48,6 +68,9 @@
                     <form action="{{ route('species.destroy', $species) }}" method="post">
                         @method('delete')
                         @csrf
+
+
+
                         <x-primary-button
                             onclick="return confirm('Are you sure you want to delete?')">Delete</x-primary-button>
                     </form>
